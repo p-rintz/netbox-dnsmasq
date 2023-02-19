816cc4787b2c3ad2a05cc4c0470834c7e872bc67
Philipp Rintz <git@rintz.net>
2023-02-19 19:40:33 +0100

    Add tests, check for duplicate macs
    - ability to have multiple domains for one IP in the DNS config via the --enable-duplicate-ips CLI parameter

--- README.md
+++ README.md
@@ -1,34 +1,59 @@
 # Netbox-dnsmasq
 Create dnsmasq DHCP/DNS configs using the Netbox API.
 
-Set your Netbox Instance as a environment variable `NETBOX_ENDPOINT` and your API token as `NETBOX_TOKEN`
+## Configuration
 
-Configs will be saved in the following two locations:  
-DHCP config location: `/etc/dnsmasq.d/dhcphosts.conf`  
-DNS hosts file location: `/etc/dnsmasq.hosts`
+**Set your Netbox Instance as a environment variable `NETBOX_ENDPOINT` and your API token as `NETBOX_TOKEN`**  
+Install the required python packages:
+```
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+**Python 3.9 or higher is required.**
 
+### Config locations
+
+Configs will, by default, be saved in the following two locations:  
+**DHCP config location**: `/etc/dnsmasq.d/dhcphosts.conf`  
+**DNS hosts file location**: `/etc/dnsmasq.hosts`
+
+### DHCP Config
 **Prefixes** that should be scanned for Hosts, by default, need to be tagged with the tag "dhcp" in Netbox.  
 IPs that should not be installed in the DHCP config or installed as DNS records need to be tagged with "no-dhcp" in Netbox.  
+You can change the DHCP tag to something else by using the `--tag` CLI parameter.
 
 If you like, you can change both tags to something else by using the `--tag` and `--dns-tag` CLI parameters. 
 
-**Adding IP's to DNS will also work for singular IPs outside prefixes.**
-
-This python program will also use other tags applied to the IP address and use them as the Set for dnsmasq.  
+This python script will also use other tags applied to the IP address and use them as the Set for dnsmasq.  
 This is important if you have multiple Vlans for example and want dnsmasq to manage the different options based on the sets.
 
-Available CLI parameters are:
+### DNS Config
+**IP's** that should be installed in the DNS config, by default, need to be tagged with the tag "no-dhcp" in Netbox.  
+You can change this tag to something else by using the `--dns-tag` CLI parameter.
+
+By default, single IP's cannot be added multiple times to the DNS config.   
+This can be changed by using the `--enable-duplicate-ips` CLI parameter.  
+Duplicate IP's will then be merged to a single line in the DNS config.
+
+Example:  
+`10.1.1.1 nonworking2.example.com duplicateip.example.com`
+
+### Available CLI parameters are:
 
 ```
-usage: netbox-dnsmasq.py [-h] [-d] [--dev] [-t TAG] [--dns-tag DNS_TAG]
+usage: netbox_dnsmasq.py [-h] [-d] [--dev] [-t TAG] [--dns-tag DNS_TAG] [-e]
 
 options:
-  -h, --help         show this help message and exit
-  -d, --debug        more verbose feedback
-  --dev              save configs to current directory and disable dnsmasq restart
-  -t TAG, --tag TAG  netbox tag that will be used to search for prefixes to include in DHCP config (default: dhcp)
-  --dns-tag DNS_TAG  netbox tag that will be used to mark IP's to only be included in DNS config (default: no-dhcp)
+  -h, --help                  show this help message and exit
+  -d, --debug                 more verbose feedback
+  --dev                       save configs to current directory and disable dnsmasq restart
+  -t TAG, --tag TAG           netbox tag that will be used to search for prefixes to include in DHCP config (default: dhcp)
+  --dns-tag DNS_TAG           netbox tag that will be used to mark IP's to only be included in DNS config (default: no-dhcp)
+  -e, --enable-duplicate-ips  enable & merge duplicate IP's in DNS config
 ```
+
+## Examples
 
 Example output could for example be the following:
 
@@ -76,6 +101,8 @@ dhcp-option = option:dns-server, 10.0.0.2
 ```
 
 ## Roadmap:
-- Test config for duplicate MACs (outside IPv4/IPv6)
-- Support multiple domains for a single IP
+- Enable setting of config file locations via CLI
+- Finish tests
+  - Test coverage is currently at 87%
+- Add CI pipeline
 - Suggestions welcome
--- netbox_dnsmasq.py
+++ netbox_dnsmasq.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import pathlib
+
 import pynetbox
 import re
 from subprocess import CalledProcessError, run
@@ -23,8 +25,8 @@ def import_config(dev: bool = False) -> tuple[str, str, str, str]:
         endpoint = os.environ['NETBOX_ENDPOINT']
         token = os.environ['NETBOX_TOKEN']
     except KeyError as e:
-        print(f'Missing Environment variable: {e}')
-        exit(1)
+        logger.error(f'Missing Environment variable: {e}')
+        raise
     if dev:
         dhcp_config = "dhcphosts.conf"
         dns_hosts = "dnsmasq.hosts"
@@ -36,12 +38,69 @@ def import_config(dev: bool = False) -> tuple[str, str, str, str]:
     return endpoint, token, dhcp_config, dns_hosts
 
 
-def get_ip_data(dhcp_ignore_tag: str, dhcp_tag: str) -> tuple[dict, dict]:
+def check_duplicates(nb_dhcp: dict, nb_dns: dict, enable_duplicates: bool) -> tuple[dict, dict]:
+    """
+    Check for duplicate MAC addresses in the data Netbox returned, warn, and remove from DHCP data.
+    If enabled via CLI parameter, enable duplicate IP addresses in DNS data.
+    Otherwise, duplicate IP's will be removed.
+    Args:
+        nb_dhcp (dict): Dictionary of DHCP data
+        nb_dns (dict): Dictionary of DNS data
+        enable_duplicates (bool): If True, duplicate IP addresses will be merged to one entry
+    Returns:
+        nb_dhcp (dict): Dictionary of DHCP data
+        nb_dns (dict): Dictionary of DNS data
+    """
+    dhcp_ips = []
+    dhcp_macs = []
+    for ip in nb_dhcp:
+        dhcp_ips.append(ip)
+        dhcp_macs.append(nb_dhcp[ip]['mac'])
+    if len(dhcp_macs) != len(set(dhcp_macs)) or len(dhcp_ips) != len(set(dhcp_ips)):
+        logger.warning("Duplicate MAC or IP addresses found. Removing from DHCP config.")
+        for k, v in nb_dhcp.copy().items():
+            if dhcp_macs.count(v['mac']) > 1 or dhcp_ips.count(k) > 1:
+                logger.info(f"Duplicate MAC found: {v['mac']} - {k} - removing from DHCP config.")
+                del dhcp_macs[dhcp_macs.index(v['mac'])]
+                del dhcp_ips[dhcp_ips.index(k)]
+                del nb_dhcp[k]
+            if len(dhcp_macs) == len(set(dhcp_macs)) and len(dhcp_ips) == len(set(dhcp_ips)):
+                break
+    dns_ips = []
+    for fqdn in nb_dns:
+        dns_ips.append(nb_dns[fqdn]['ip'])
+    if len(dns_ips) != len(set(dns_ips)):
+        logger.warning("Duplicate IP addresses in DNS config found.")
+        for k, v in nb_dns.copy().items():
+            if dns_ips.count(v['ip']) > 1:
+                if not enable_duplicates:
+                    logger.info(f"Duplicate IP address {v['ip']} found. Removing from DNS config.")
+                    del dns_ips[dns_ips.index(v['ip'])]
+                    del nb_dns[k]
+                else:
+                    if k in nb_dns:
+                        logger.info(f"Duplicate IP address {v['ip']} found. "
+                                    f"Related FQDN's will be merged into one entry.")
+                        for k2, v2 in nb_dns.copy().items():
+                            if v2['ip'] == v['ip'] and k2 != k:
+                                logger.info(f"Merging {k2} with {k}")
+                                if 'secondary' not in nb_dns[k]:
+                                    nb_dns[k]['secondary'] = [k2]
+                                else:
+                                    nb_dns[k]['secondary'].append(k2)
+                                del nb_dns[k2]
+            if len(dns_ips) == len(set(dns_ips)):
+                break
+    return nb_dhcp, nb_dns
+
+
+def get_ip_data(dhcp_ignore_tag: str, dhcp_tag: str, nb: pynetbox.api) -> tuple[dict, dict]:
     """
     Get IP&DNS data from Netbox and return a dictionary of DHCP and DNS data.
     Args:
         dhcp_ignore_tag (str): Netbox tag to ignore IP addresses
         dhcp_tag (str): Netbox tag to include IP addresses
+        nb (pynetbox.api): Netbox API object
     Returns:
         dhcp_data (dict): Dictionary of DHCP data
         dns_data (dict): Dictionary of DNS data
@@ -53,18 +112,18 @@ def get_ip_data(dhcp_ignore_tag: str, dhcp_tag: str) -> tuple[dict, dict]:
     for ip in ip_addresses:
         ip_address = re.sub('/.*', '', ip.address)
         if len(ip.dns_name) > 0:
-            nb_dns.update({ip_address: {'tags': [], 'hostname': ''}})
+            nb_dns.update({ip.dns_name: {'tags': [], 'ip': ''}})
         else:
-            logger.warning(f"Warning: {ip_address} has {dhcp_ignore_tag} tag, but no FQDN.")
+            logger.warning(f"{ip_address} has {dhcp_ignore_tag} tag, but no FQDN.")
             continue
         for tag in ip.tags:
             if ip.dns_name:
-                nb_dns[ip_address]['tags'].append(tag)
+                nb_dns[ip.dns_name]['tags'].append(str(tag))
         if dhcp_tag in list(t.name for t in ip.tags):
-            logger.warning(f"Warning: {ip} has {dhcp_tag} tag, but also has {dhcp_ignore_tag} tag.")
+            logger.warning(f"{ip} has {dhcp_tag} tag, but also has {dhcp_ignore_tag} tag.")
         if len(ip.dns_name) > 0:
             if ip.dns_name:
-                nb_dns[ip_address]['hostname'] = ip.dns_name
+                nb_dns[ip.dns_name]['ip'] = ip_address
                 logger.debug(f'Added DNS Host {ip.dns_name} with IP {ip_address}')
     for prefix in dhcp_prefixes:
         prefix_ips = nb.ipam.ip_addresses.filter(parent=prefix.prefix)
@@ -73,6 +132,10 @@ def get_ip_data(dhcp_ignore_tag: str, dhcp_tag: str) -> tuple[dict, dict]:
             if ip.assigned_object_id:
                 if ip.assigned_object.mac_address is not None and dhcp_ignore_tag not in list(t.name for t in ip.tags):
                     mac = ip.assigned_object.mac_address.lower()
+                    if ip_address in nb_dhcp:
+                        logger.warning(f"Duplicate IP address {ip_address}"
+                                       f" with MAC {mac} found in DHCP config. Skipping.")
+                        continue
                     nb_dhcp.update({ip_address: {'mac': '', 'tags': [], 'hostname': ''}})
                     nb_dhcp[ip_address]['mac'] = mac
                     logger.debug(f'Added DHCP host with MAC {mac} and IP {ip_address}')
@@ -116,44 +179,47 @@ def format_dhcp(data: dict) -> list:
 
 def format_dns(data: dict) -> list:
     """
-    Format DNS data for dnsmasq
+    Format DNS data for dnsmasq.
+    If a host has secondary FQDN's, they will be appended to the primary FQDN.
     Args:
         data (dict): Dictionary of DNS data from Netbox
     Returns:
         dnslist (list): List of DNS config lines
     """
     dns_list = []
-    for ip in data:
-        host = [ip]
-        if len(data[ip]['hostname']) > 0:
-            host.append(f" {data[ip]['hostname']}")
+    for hostname in data:
+        if 'secondary' in data[hostname]:
+            secondaries = ' '.join(data[hostname]['secondary'])
+            merged_name = f"{hostname} {secondaries}"
+            host = f'{data[hostname]["ip"]} {str(merged_name)}\n'
         else:
-            continue
-        host.append("\n")
-        host = ''.join(host)
+            host = f'{data[hostname]["ip"]} {str(hostname)}\n'
+
         dns_list.append(host)
     return dns_list
 
 
-def write_config(dhcplist: list, dnslist: list) -> None:
+def write_config(dhcplist: list, dnslist: list, dns_loc: str | pathlib.Path, dhcp_loc: str | pathlib.Path) -> None:
     """
     Write DHCP and DNS config files.
     Args:
         dhcplist (list): List of DHCP config lines
         dnslist (list): List of DNS config lines
+        dns_loc (str): Location to write DNS config file to
+        dhcp_loc (str): Location to write DHCP config file to
     Returns:
         None
     """
     try:
-        dhcpconfig = open(DHCP_config_location, "w")
+        dhcpconfig = open(dhcp_loc, "w")
         dhcpconfig.writelines(dhcplist)
         dhcpconfig.close()
-        dnsconfig = open(DNS_hosts_location, "w")
+        dnsconfig = open(dns_loc, "w")
         dnsconfig.writelines(dnslist)
         dnsconfig.close()
     except PermissionError as e:
         logger.error(f'Received error "{e.strerror}" while writing config {e.filename}')
-        exit(1)
+        raise
 
 
 def restart_service() -> None:
@@ -181,17 +247,20 @@ if __name__ == "__main__":
     parser.add_argument('--dns-tag', default='no-dhcp',
                         help="netbox tag that will be used to mark IP's"
                              " to only be included in DNS config (default: no-dhcp)")
+    parser.add_argument('-e', '--enable-duplicate-ips', action='store_true',
+                        help="enable & merge duplicate IP's in DNS config")
     args = parser.parse_args()
 
     logger.remove(0)
     logger.add(sys.stderr, level="DEBUG" if args.debug else "INFO")
     NETBOX_ENDPOINT, NETBOX_TOKEN, DHCP_config_location, DNS_hosts_location = import_config(args.dev)
 
-    nb = pynetbox.api(url=NETBOX_ENDPOINT, token=NETBOX_TOKEN)
+    api = pynetbox.api(url=NETBOX_ENDPOINT, token=NETBOX_TOKEN)
 
-    dhcp, dns = get_ip_data(args.dns_tag, args.tag)
+    dhcp, dns = get_ip_data(args.dns_tag, args.tag, nb=api)
+    check_duplicates(nb_dhcp=dhcp, nb_dns=dns, enable_duplicates=args.enable_duplicate_ips)
     dhcp_data = format_dhcp(dhcp)
     dns_data = format_dns(dns)
-    write_config(dhcp_data, dns_data)
+    write_config(dhcplist=dhcp_data, dnslist=dns_data, dns_loc=DNS_hosts_location, dhcp_loc=DHCP_config_location)
     if not args.dev:
         restart_service()
